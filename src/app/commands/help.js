6e895082fe1d5829e070dd20370fc498da4a677d
DraX <drax@MBP-Kozloff.lan>
2023-06-13 14:59:44 -0700

    Update help command text

--- src/app/commands/help.js
+++ src/app/commands/help.js
@@ -1,6 +1,7 @@
 const help = async (message, game) => {
   const text = `The following commands are supported:
   * /help - shows current help
+  * /score @player - shows the score of the mentioned player
   * /start - starts new game (the current game will be discarded)
   * /stop - stops the current game`;
 
